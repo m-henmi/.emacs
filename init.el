6a8ff01ff81b35f1b0f5b4afe42896d489844ab0
m-henmi <m.henmi@gmail.com>
2015-12-15 08:21:34 +0900

    個人設定を myconf/myconf.el に分離

--- init.el
+++ init.el
@@ -573,32 +573,13 @@
 ;;; 個人の追加設定                                                  ;;;
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
-;;; パッケージの設定
-(require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+;;; 個人設定を別ファイルに分離
+(setq load-path
+      (append '(
+                "~/.emacs.d/myconf"
+                ) load-path))
 
-;;; 右から左に読む言語に対応させないことで描画高速化
-(setq-default bidi-display-reordering nil)
-
-;;; splash screenを無効にする
-(setq inhibit-splash-screen t)
-
-;;; 同じ内容を履歴に記録しないようにする
-(setq history-delete-duplicates t)
-
-;; ツールバー非表示
-(tool-bar-mode 0)
-
-;; スクリーンを再描画してカレント行を中央に置く
-(global-set-key "\C-l" 'recenter)
-
-;; C-h でバックスペース
-(keyboard-translate ?\C-h ?\C-?)
-
-;;; モードラインに時刻を表示する
-(display-time)
+(load "myconf")     ;; 個人用設定ファイル
 
 
 ;; Local Variables:
